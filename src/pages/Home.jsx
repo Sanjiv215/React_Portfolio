20e4abce7adb61a51fd6e9f240a54991ecde7172
Sanjiv Prasad <sanjiv@Sanjivs-MacBook-Air.local>
2026-04-17 23:24:05 +0530

    Components names changed to .jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Hero from '../components/Hero/Hero'
-import Skills from "../components/Skills/Skills"
-import Projects from "../components/Projects/Projects"
-import Journey from "../components/Journey/Journey"
+import Navbar from '../components/Navbar/Navbar.jsx'
+import Hero from '../components/Hero/Hero.jsx'
+import Skills from "../components/Skills/Skills.jsx"
+import Projects from "../components/Projects/Projects.jsx"
+import Journey from "../components/Journey/Journey.jsx"
 
 const Home = () => {
     return (
